46806fd58dabf2a44f765c23d2243128ef2459ba
Mariam Tajudeen Sulaiman <marytechtajudeen@gmail.com>
2024-02-05 21:27:41 +0100

    coding javascrpt

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function showInput(){
     document.querySelector('input').style.display = "block";
 }
+const toggle = document.getElementById('toggle');
+toggle.onclick = function(){
+    toggle.classList.toggle('active');
+}
