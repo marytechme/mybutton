2460a0fdcf3a3ffca9872afba7b8f4ef729cf887
Mariam Tajudeen Sulaiman <marytechtajudeen@gmail.com>
2024-02-05 22:08:50 +0100

    I am stucked

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function showInput(){
     document.querySelector('input').style.display = "block";
 }
 const toggle = document.getElementById('toggle');
-toggle.onclick = function(){
-    toggle.classList.toggle('active');
+toggle.onclick()= function(){
+    toggle.classList.toggle('active')
 }
